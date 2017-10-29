008385fd54aefd7de27202d9794b2c309e26f6b3
Julio Velarde <cevegaju@hotmail.com>
2017-10-29 05:59:49 -0500

    Tarea completa.

--- Cibertec/Cibertec.Repositories.Dapper/Northwind/NorthwindUnitOfWork.cs
+++ Cibertec/Cibertec.Repositories.Dapper/Northwind/NorthwindUnitOfWork.cs
@@ -7,10 +7,24 @@ namespace Cibertec.Repositories.Dapper.Northwind
     {
         public NorthwindUnitOfWork(string connectionString)
         {
-            Customers = new CustomerRepository(connectionString);            
+            Customers = new CustomerRepository(connectionString);
+            Orders = new OrderRepository(connectionString);
+            OrderItems = new OrderItemRepository(connectionString);
+            Products = new ProductRepository(connectionString);
+            Suppliers = new SupplierRepository(connectionString);
+            Users = new UserRepository(connectionString);
         }
 
         public ICustomerRepository Customers { get; private set; }
-        
+
+        public IOrderRepository Orders { get; private set; }
+
+        public IOrderItemRepository OrderItems { get; private set; }
+
+        public IProductRepository Products { get; private set; }
+
+        public ISupplierRepository Suppliers { get; private set; }
+
+        public IUserRepository Users { get; private set; }
     }
 }
---
+++ Cibertec/Cibertec.Repositories/Northwind/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Cibertec.Models;
+
+namespace Cibertec.Repositories.Northwind
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        User ValidaterUser(string email, string password);
+    }
+}
