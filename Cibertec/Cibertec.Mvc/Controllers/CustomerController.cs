0d9b4cb2a452f4a66d8b0c6fe8c6b69c9560175e
Julio Velarde <cevegaju@hotmail.com>
2017-10-29 05:53:42 -0500

    Uso de repositorio en el controlador.

---
+++ Cibertec/Cibertec.Mvc/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Cibertec.Repositories.Dapper.Northwind;
+using Cibertec.UnitOfWork;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace Cibertec.Mvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IUnitOfWork _unit;
+        public CustomerController()
+        {
+            _unit = new NorthwindUnitOfWork(ConfigurationManager.ConnectionStrings["NorthwindConnection"].ToString());
+        }
+        public ActionResult Index()
+        {
+            return View(_unit.Customers.GetList());
+        }
+    }
+}
