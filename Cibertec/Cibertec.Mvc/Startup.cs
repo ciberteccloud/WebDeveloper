980e9d8d34a966daa222484a09da6f30479dea3f
Julio Velarde <cevegaju@hotmail.com>
2017-10-29 04:37:19 -0500

    Laboratorio 1 completo

---
+++ Cibertec/Cibertec.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cibertec.Mvc.Startup))]
+namespace Cibertec.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
