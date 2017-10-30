3280004ea1426659ee0a25bd7121c44e7d82c869
Julio Velarde <cevegaju@hotmail.com>
2017-10-30 14:23:03 -0500

    Laboratorio 02 completo.

--- Cibertec/Cibertec.Mvc/Controllers/CustomerController.cs
+++ Cibertec/Cibertec.Mvc/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Cibertec.UnitOfWork;
+﻿using Cibertec.Models;
+using Cibertec.UnitOfWork;
 using System.Web.Mvc;
 
 namespace Cibertec.Mvc.Controllers
@@ -13,6 +14,50 @@ namespace Cibertec.Mvc.Controllers
         public ActionResult Index()
         {
             return View(_unit.Customers.GetList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _unit.Customers.Insert(customer);
+                RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(_unit.Customers.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _unit.Customers.Update(customer);
+                RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(_unit.Customers.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Customer customer)
+        {
+            if (_unit.Customers.Delete(customer)) return RedirectToAction("Index");
+            return View(customer);
         }
     }
 }
