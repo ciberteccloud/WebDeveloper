69c9d468d30e0a38b8eaae06755272ebce0c9952
Cesar Velarde <juvega@gmail.com>
2017-11-27 12:05:21 -0500

    Proyecto de Net Core 2.0

---
+++ Cibertec.NetCore/Cibertec.Repositories.Dapper/Northwind/NorthwindUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using Cibertec.Repositories.Northwind;
+using Cibertec.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cibertec.Repositories.Dapper.Northwind
+{
+    public class NorthwindUnitOfWork : IUnitOfWork
+    {
+        public NorthwindUnitOfWork(string connectionString)
+        {
+            Customers = new CustomerRepository(connectionString);
+            OrderItems = new OrderItemRepository(connectionString);
+            Orders = new OrderRepository(connectionString);
+            Products = new ProductRepository(connectionString);
+            Suppliers = new SupplierRepository(connectionString);
+            Users = new UserRepository(connectionString);
+        }
+
+        public ICustomerRepository Customers { get; private set; }
+
+        public IOrderItemRepository OrderItems { get; private set; }
+
+        public IOrderRepository Orders { get; private set; }
+
+        public IProductRepository Products { get; private set; }
+
+        public ISupplierRepository Suppliers { get; private set; }
+
+        public IUserRepository Users { get; private set; }
+    }
+}
